14d777b83fe2a811d24250d0f5ce4a9fe08bdeb1
ozwaldo <ozwaldo@gmail.com>
2019-02-08 12:51:20 -0600

    8/02/2019

--- app/src/main/java/com/example/micontentprovider/MainActivity.java
+++ app/src/main/java/com/example/micontentprovider/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.micontentprovider;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG_MAIN = MainActivity.class.getSimpleName();
+
     TextView textView;
 
     @Override
@@ -18,13 +22,66 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
     }
 
-    public void onClickBotonoes(View view) {
+    public void onClickBotones(View view) {
+
+        String queryUri = Contract.CONTENT_URI.toString();
+
+        Log.d(TAG_MAIN, queryUri);
+
+        String[] projection = new String[]{Contract.CONTENT_PATH};
+
+        String selectionClause;
+        String selectionArgs[];
+
         switch (view.getId()) {
             case R.id.btn_mostrar_primera:
-                textView.append("\n Boton1.");
+                // SELECT * FROM asignatura WHERE ASIGNATURA_ID = 0
+                selectionClause = Contract.ASIGNATURA_ID + " = ?";
+                selectionArgs = new String[]{"0"};
+                //textView.append("\n Boton1.");
                 break;
             case R.id.btn_mostrar_todos:
-                textView.append("\n Boton2.");
+                selectionClause = null;
+                selectionArgs = null;
+                //textView.append("\n Boton2.");
+                break;
+            default:
+                selectionClause = null;
+                selectionArgs = null;
         }
+
+        Cursor cursor = getContentResolver().query(
+                Uri.parse(queryUri),
+                projection,
+                selectionClause,
+                selectionArgs,
+                null
+        );
+
+        if (cursor != null) {
+            if (cursor.getCount() > 0) {
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(projection[0]);
+                do {
+                    String asignatura = cursor.getString(columnIndex);
+                    textView.append(asignatura + "\n");
+                } while (cursor.moveToNext());
+            } else {
+                textView.append("No existen asignaturas.");
+            }
+        } else {
+            textView.append("No existen Asignaturas.");
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
